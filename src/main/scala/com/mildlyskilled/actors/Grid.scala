47729f501f2bb5f43ee9d661bd10aac46ce54ff6
Kwabena Aning <kwabena.aning@gmail.com>
2015-02-21 14:11:01 +0000

    Tests for grid and cell are done for now

--- src/main/scala/com/mildlyskilled/actors/Grid.scala
+++ src/main/scala/com/mildlyskilled/actors/Grid.scala
@@ -1,11 +1,12 @@
 package com.mildlyskilled.actors
 
 case class Grid() {
-  def grid = List((1, 'A'), (2, 'B'), (3, 'C'), (4, 'D'), (5, 'E'), (6, 'F'),(7, 'G'), (8, 'H'), (9, 'I'), (10, 'J'))
 
   def getRows: Int = 1
 
   def getColumns: Int = 1
+
+  def build: Boolean = ???
 
   def getCells: List[Cell] = ???
 
