7630d305827d2ded3c0e4b09bf9da16231d7fa93
rajan patel <djcoder@kikuconsultancy.co.uk>
2015-02-21 13:46:10 +0000

    Add cell test for coordinates

--- src/test/scala/CellTestSuite.scala
+++ src/test/scala/CellTestSuite.scala
@@ -1,5 +1,15 @@
+import com.mildlyskilled.actors.Cell
 import org.scalatest.FunSuite
 
 class CellTestSuite extends FunSuite{
+
+  val aCell = new Cell('A',1)
+
+  test("A cell must have coordinates") {
+    assert(aCell.coordinates != null)
+    assert(aCell.coordinates._1 == 'A')
+    assert(aCell.coordinates._2 == 1)
+
+  }
 
 }
