2f09d4a93c080dcfdf680ef652cda190dcbcb4cc
Kwabena Aning <kwabena.aning@gmail.com>
2015-03-14 16:35:35 +0000

    Guard to make sure players can actually play

--- src/main/scala/com/mildlyskilled/actors/BattleshipServer.scala
+++ src/main/scala/com/mildlyskilled/actors/BattleshipServer.scala
@@ -8,18 +8,21 @@ import scala.collection.mutable.Map
 class BattleshipServer extends Actor {
   val connectedPlayers:Map[ActorRef, Player] = Map()
 
+  def canPlay = connectedPlayers.size == 2
+
   def receive = {
     case BeginGame => println("Game starting")
     case ConnectClient(player) => {
       connectedPlayers += (sender() -> player)
       sender() ! InfoMessage("You're connected")
-      if (connectedPlayers.size == 2) {
+      if (canPlay) {
         connectedPlayers.foreach({ case (r, _) => r ! InfoMessage("Ready to play")})
         connectedPlayers.keys.head ! YourTurn
       }
     }
     case Play(x: Int, y:Int) => {
-      connectedPlayers.keys.filter(_ != sender()).foreach(r => r ! FireOnCell(x, y))
+      if(canPlay) connectedPlayers.keys.filter(_ != sender()).foreach(r => r ! FireOnCell(x, y))
+      else sender() ! "Game not ready"
     }
   }
 }
